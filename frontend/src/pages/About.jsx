eb1401b014e947bda7d761fb3a85c637c571075d
jenjose72 <jenjose72@gmail.com>
2025-12-10 11:30:21 +0530

    latest UI

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -279,7 +279,7 @@ function VenueSection() {
     {
       icon: Bus,
       title: "Nearest Bus Station",
-      name: "Kottaramattom Bus Stand",
+      name: "Pala Bus Stand",
       distance: "7.5 km away",
       code: "Pala ",
       mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125845.51208799866!2d76.53330041640626!3d9.708988299999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07cd397a01708b%3A0x5835c896e385614b!2sKottaramattom%20Private%20Bus%20Stand!5e0!3m2!1sen!2sin!4v1765088191992!5m2!1sen!2sin"
